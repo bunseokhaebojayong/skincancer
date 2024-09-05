66b63ae65594a2b895c05f8cfc2d20aeabf817d4
Leo.Lee <bonaparte19th@hotmail.com>
2024-09-05 15:54:31 +0000

    run.sh 수정

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-accelerator launch train.py \
+accelerate launch --multi_gpu train.py \
 --seed 22 \
 --model_name "tf_efficientnetv2_m.in21k" \
 --img_size 224 \
 --num_epoch 50 \
---t_batch_size 8 \
---v_batch_size 16 \
+--t_batch_size 64 \
+--v_batch_size 64 \
 --lr 1e-4 \
 --lr_scheduler "cosine" \
 --min_lr 1e-6 \
@@ -13,5 +13,5 @@ accelerator launch train.py \
 --fold 0 \
 --n_fold 5 \
 --t_0 12 \
---n_accumulate 4
+--n_accumulate 1
 
